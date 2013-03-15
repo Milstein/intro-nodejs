9c23539d70b99496df8530591d30f2b62a68fde3
andrewhanson <andrew@codinginertia.com>
2013-03-15 12:36:21 -0600

    updated azure example

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var port = process.env.PORT || 9995;
 
 http.createServer(function(req, res){
    res.writeHead(200, {'Content-Type': 'text/plain'});
-   res.end('Hello from Azure! \n');
+   res.end('Hello Boise Code Camp ! \n');
 }).listen(port);
 console.log("listening on port: " + port);
 
