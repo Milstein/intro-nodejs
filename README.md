e2654308a64ed86344825ded8cf96a02235f0073
andrewhanson <andrew@codinginertia.com>
2013-03-15 04:00:21 -0600

    added slides

---
+++ README.md
@@ -0,0 +1,4 @@
+bcc-intro-nodejs
+================
+
+Boise Code Camp - Intro to Node.js
---
+++ examples/square.js
@@ -0,0 +1,7 @@
+exports.area = function (s) {
+  return s * s;
+};
+
+exports.circumference = function (r) {
+  return 4 * s;
+};
